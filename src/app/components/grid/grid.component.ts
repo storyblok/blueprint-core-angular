c3bae3311550e7c2747d7f552551facdf6dba979
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2026-04-28 17:06:33 +0530

    chore: update angular package to latest and using  sb-component

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
-import { SbBlokDirective, type SbBlokData } from '@storyblok/angular';
+import { StoryblokComponent, type SbBlokData } from '@storyblok/angular';
 
 export interface GridBlok extends SbBlokData {
   columns?: SbBlokData[];
@@ -8,12 +8,10 @@ export interface GridBlok extends SbBlokData {
 @Component({
   selector: 'app-grid',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [SbBlokDirective],
+  imports: [StoryblokComponent],
   template: `
     <div class="grid">
-      @for (column of blok().columns ?? []; track column._uid) {
-        <ng-container [sbBlok]="column" />
-      }
+      <sb-component [sbBlok]="blok().columns" />
     </div>
   `,
 })
