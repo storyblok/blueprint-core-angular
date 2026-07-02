5d2008046433a52c8a1466bc0a4b6a3536712d79
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2026-07-02 18:50:30 +0530

    chore: add as const so there are no type issue

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   accessToken: 'STORYBLOK_DELIVERY_API_TOKEN', // Replace with your Storyblok Delivery API token
-  region: 'STORYBLOK_REGION', // Replace with your Storyblok region
+  region: 'STORYBLOK_REGION' as const, // Replace with your Storyblok region (e.g. "eu", "us", or "ap")
 };
